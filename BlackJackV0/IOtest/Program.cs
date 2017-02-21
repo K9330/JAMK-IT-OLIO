b4647982dcb3cb55d665c777bd96bb4973bb266a
k9330 <k9330@ICT-421-WS-05>
2017-02-21 16:51:53 +0200

    uptade new project

---
+++ BlackJackV0/IOtest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOtest
+{
+
+    class Program
+    {
+        static void TestaataanKirjoitin()
+        {
+            System.IO.StreamWriter outputFile = null;
+            try
+            {
+                outputFile = new System.IO.StreamWriter(@"Test.txt");
+                outputFile.WriteLine("Here is a sample text to file.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Can't open file for writing (UnauthorizedAccessException)");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Opened stream is null (ArgumentNullException)");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Opened stream is not writable (ArgumentException)");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("An IO error happend (IOException)");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Some other exception happend (Exception)");
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestaataanKirjoitin();
+        }
+    }
+}
--- BlackJackV0/Viiko-tehtava05/Program.cs
+++ BlackJackV0/Viiko-tehtava05/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using JAMK.IT;
 namespace Viiko_tehtava05
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Lab05Teht01();
         }
+        static void Tehtava1()
+        {
+
+        }
+        static void Lab05Teht01()
+        {
+            //testataan tyre ja vehecle luokkia
+
+            //luodaan ensimmäinen ajoneuvo
+            Vehicle auto = new JAMK.IT.Vehicle();
+            auto.Brand = "BMW";
+            auto.Model = "320";
+            //ostetaan renkaida
+            Tyre rinkula = new Tyre();
+            rinkula.Branch = "Hankook";
+            rinkula.Size = "205/55r16";
+            //laitetaan renkaad
+            auto.AddTyre(rinkula);
+            auto.AddTyre(rinkula);
+            auto.AddTyre(rinkula);
+            auto.AddTyre(rinkula);
+
+            Console.WriteLine("autosi {0} on seuraavat tiedot: {1}", auto.Brand, auto.ToString());
+        }
+
+
     }
+
 }
